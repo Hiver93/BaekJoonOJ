71f52213cc3190e3498a13c8a983976e60e8ab0a
Hiver93 <90398836+Hiver93@users.noreply.github.com>
2022-04-09 10:53:40 +0900

    NumberTheoryAndCombinatorics_11050_BinomialCoefficient
    해결
    https://www.acmicpc.net/problem/11050

---
+++ NumberTheoryAndCombinatorics/NumberTheoryAndCombinatorics_11050_BinomialCoefficient/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int Factorial(int n)
+{
+	int result = 1;
+	for (int i = n; i > 1; i--)
+	{
+		result *= i;
+	}
+
+	return result;
+}
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+
+	if (k == 0)
+		cout << 0;
+	else
+		cout << Factorial(n) / (Factorial(k) * Factorial(n - k));
+}
