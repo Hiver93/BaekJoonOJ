5e6f9a1e5f52fe06fa4e6ac702dabd29365af7e1
Hiver93 <90398836+Hiver93@users.noreply.github.com>
2022-09-22 21:32:07 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int min = 2147000000;
+    int max = 0;
+    cin >> n;
+    int input;
+    if(n==1)
+    {
+        cin >> input;
+        cout << input*input;
+        return 0;
+    }
+    for(int i =0; i < n; i++)
+    {
+        cin >> input;
+        if(input < min)
+            min = input;
+        if(max < input)
+            max = input;
+    }
+    cout << min*max;
+    return 0;
+    
+    
+}
