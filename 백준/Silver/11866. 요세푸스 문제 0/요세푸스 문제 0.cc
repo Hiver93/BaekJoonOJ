1429020eb23e1daf16809e870fbb4c5eae2d2e6e
Hiver93 <90398836+Hiver93@users.noreply.github.com>
2023-01-08 23:07:59 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -1,30 +1,14 @@
 #include <iostream>
-#include <stack>
-#include <queue>
-#include <list>
 using namespace std;
-
-
-
 
 int main()
 {
-	/*stack<int> s;
-	s.push(2);
-	s.push(3);
-	cout << s.top();
-	s.pop();
-
-	cout << s.size();*/
-
-	//queue<int> q;
-
 	int q[1001];
+	int ans[1000];
+	int idx = 0;
 	int front = 0;
 	int rear = 0;
-	int arr[1000];
-	int idx = 0;
-	
+
 	int n;
 	int k;
 	cin >> n >> k;
@@ -32,30 +16,26 @@ int main()
 	{
 		q[rear++] = i + 1;
 	}
-
+	
 	int cnt = 1;
 	while (front != rear)
 	{
-		int tmp = q[(front++) % 1001];
-		if (cnt % k != 0)
-		{
-			q[(rear++) % 1001] = tmp;
+		int tmp = q[front++];
+		if (cnt % k == 0) {			
+			ans[idx++] = tmp;
 		}
-		else
-		{
-			arr[idx++] = tmp;
+		else {
+			q[rear++] = tmp;
 		}
+
 		front %= 1001;
 		rear %= 1001;
 		cnt++;
 	}
-
 	cout << "<";
-	for (int i = 0; i < n-1; ++i)
+	for (int i = 0; i < n - 1; ++i)
 	{
-		cout << arr[i] << ", ";
+		cout << ans[i] << ", ";
 	}
-	cout << arr[n - 1] << ">";
-
-	
+	cout << ans[n - 1] << ">";
 }
