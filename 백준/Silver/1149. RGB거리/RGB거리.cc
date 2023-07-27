964736cdc1f373d8b11efb462fd089b8eaebee7f
Hiver93 <90398836+Hiver93@users.noreply.github.com>
2023-07-27 17:24:43 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,53 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int arr[1000][3];
-int check[1000][3];
-int ans = 1000000;
-
-void Func(int current, int n)
-{
-	if (current == n)
-	{
-		return;
-	}
-	for (int i = 0; i < 3; i++)
-	{		
-		int temp = 0;
-		for (int j = 0; j < 3; j++)
-		{
-			if (j == i)
-				continue;
-			if (!temp)
-				temp = arr[current][i] + check[current - 1][j];
-			else
-				if (temp > arr[current][i] + check[current - 1][j])
-					check[current][i] = arr[current][i] + check[current - 1][j];
-				else
-					check[current][i] = temp;
-		}		
-	}
-	Func(current + 1, n);
-	
-}
+// 1. 1번집의 색은 2번집과 달라야 한다.
+// 2. N번 집의 색은 N-1번 집의 색과 달라야 한다.
+// 3. i(2 <= i <= N-1)번 집의 색은 i-1번, i+1번 집이 색과 달라야 한다.
+// 빨강 초록 파랑 세가지 비용 집마다 다름
+// 모든 집을 칠하는 비용의 최솟값을 출력
 
 int main()
 {
+	// 해당 인덱스의 집을 각 색깔로 칠했을 경우 최소비용을 메모해둘 배열
+	int red[1001]{0};
+	int blue[1001]{0};
+	int green[1001]{0};
 	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
-	}
-	check[0][0] = arr[0][0];
-	check[0][1] = arr[0][1];
-	check[0][2] = arr[0][2];
 
-	Func(1, n);
-	if (check[n-1][0] <= check[n - 1][1] && check[n - 1][0] <= check[n - 1][2])
-		cout << check[n - 1][0];
-	else if (check[n - 1][1] <= check[n - 1][0] && check[n - 1][1] <= check[n - 1][2])
-		cout << check[n-1][1];
-	else
-		cout << check[n-1][2];
+	// 각 색깔로 칠했을 경우 최소비용을 구한다 (다른 색깔의 i-1 집 중 더 저렴한 비용에 현재 비용을 더하면 됨)
+	for (int i = 1; i <= n; ++i)
+	{
+		int r, g, b;
+		cin >> r >> g >> b;
+		red[i] = blue[i - 1] < green[i - 1] ? blue[i - 1] + r : green[i - 1] + r;
+		blue[i] = red[i - 1] < green[i - 1] ? red[i - 1] + b : green[i - 1] + b;
+		green[i] = red[i - 1] < blue[i - 1] ? red[i - 1] + g : blue[i - 1] + g;
+	}
+
+	// n번 인덱스 중 가장 저렴한 것으로 출력
+	int ans = red[n] < blue[n] ? red[n] : blue[n];
+	cout << ((ans < green[n]) ? ans : green[n]);
 }
