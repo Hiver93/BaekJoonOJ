8203c42fb3838dafe172f9c7d0eb6e0320725570
Hiver93 <90398836+Hiver93@users.noreply.github.com>
2023-08-30 21:32:42 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2184 ms, Memory: 100456 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -1,83 +1,26 @@
-// 뒤에서 접근 작아지면 숫자변경하고 뒤에 친구들 확인 커지면 새로 
 #include <iostream>
-#include <stack>
+#include <queue>
+#include <vector>
 using namespace std;
 
-int arr[5000000]{ 0 };
-stack <pair<int, int>> s;
-int n, l;
-
-// 앞에서 뒤로 합칠 수 있는 지 확인하여 합치기
-void CheckStack(int num, int& len)
-{
-	while (s.top().second != -1 && len < l)
-	{
-		if (num < s.top().first)
-		{
-			if (l - len <= s.top().second)
-			{
-				s.top().second -= (l - len);
-				len = l;
-			}
-			else
-			{
-				len += s.top().second;
-				s.pop();
-			}
-		}
-		else
-		{
-			break;
-		}
-		if (s.top().second == 0)
-			s.pop();
-	}
-}
 
 int main()
 {
-	cin >> n >> l;
-	for (int i = 0; i < n; ++i)
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	int n, m;
+	cin >> n >> m;
+	for (int i = 1; i <= n; ++i)
 	{
-		scanf("%d",&arr[i]);
-	}
-
-	// stack의 가장 아래 (코드 편하기 짜기 위함), 그리고 가장 뒤 숫자를 넣어두고 시작
-	s.push({ -2147483648,-1 });
-	int num = arr[n-1];
-	int len = 1;
-	
-
-	// 뒤에서 앞으로
-	for (int i = n - 2; i >= 0; --i)
-	{		
-		if (len == l) // 길이가 최대가 되었다면 스택에 하나만 일단 집어넣는다. 길이 -1
+		int tmp;
+		cin >> tmp;
+		pq.push({ tmp,i });
+		while (i - m >= pq.top().second)
 		{
-			s.push({ num,1 });
-			len--;
+			pq.pop();
 		}
-		if (num < arr[i]) // 뒤의 것이 앞것보다 작다면 스택에 집어넣고 새로
-		{
-			s.push({ num,len });
-			num = arr[i];
-			len = 1;
-		}
-		else// 뒤의 것이 앞 것보다 크다면, 숫자 바꾸고 합칠 수 있는 지 체크
-		{
-			num = arr[i];
-			len++;
-			CheckStack(num, len);
-		}
-	}
-	s.push({ num, len });
-
-	// 출력
-	while (s.top().second != -1)
-	{
-		for (int i = 0; i < s.top().second; ++i)
-		{
-			printf("%d ", s.top().first);
-		}
-		s.pop();
+		printf("%d ", pq.top().first);
 	}
 }
