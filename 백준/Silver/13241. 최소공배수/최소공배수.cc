eac55372261271e6f0be7c8aa1d68e4b7b1b0f97
Hiver93 <90398836+Hiver93@users.noreply.github.com>
2023-09-07 17:40:51 +0900

    [Silver V] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int fun(int a, int b) {
+
+	if (a % b == 0)
+		return b;
+
+	return fun(b, a % b);
+}
+
+int main()
+{
+	long long a, b;
+	cin >> a >> b;
+	long long num = (a < b) ? fun(b, a) : fun(a, b);
+	
+	cout << num * a * b / num / num;
+}
