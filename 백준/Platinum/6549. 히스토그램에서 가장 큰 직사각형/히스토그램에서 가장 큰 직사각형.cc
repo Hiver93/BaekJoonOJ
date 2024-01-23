66e5d46eb699cca76f4c0a213a9d87631d0ceddd
Hiver93 <90398836+Hiver93@users.noreply.github.com>
2024-01-23 22:27:01 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 52 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector <pair<int, int>> v(100000);
+
+
+// 병합정렬과 같은 원리로 합치면 될듯
+void merge(int left, int mid, int right) {
+	int cnt = 0;
+	int last = 2147483647;
+	int idx = mid + 1;
+
+	for (int i = mid; i >= left; --i) {
+		int cur = v[i].first;
+		if (last < cur) {
+			continue;
+		}
+		last = cur;
+		v[i].second += cnt;
+		for (idx; idx < right + 1; ++idx) {
+			if (v[idx].first < cur) {
+				break;
+			}
+			cnt++;
+			v[i].second++;
+		}
+	}
+	cnt = 0;
+	last = 2147483647;
+	idx = mid;
+	for (int i = mid + 1; i < right + 1; ++i) {
+		int cur = v[i].first;
+		if (last < cur) {
+			continue;
+		}
+		last = cur;
+		v[i].second += cnt;
+		for (idx; idx >= left; --idx) {
+			if (v[idx].first < cur) {
+				break;
+			}
+			cnt++;
+			v[i].second++;
+		}
+	}
+}
+
+void func(int left, int right) {
+	if (left < right) {
+		int mid = (left + right) / 2;
+		func(left, mid);
+		func(mid + 1, right);
+		merge(left, mid, right);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	while (1) {
+		int  n;
+		cin >> n;
+		if (!n)
+			break;
+		for (int i = 0; i < n; ++i) {
+			int tmp;
+			cin >> tmp;
+			v[i].first = tmp;
+			v[i].second = 1;
+		}
+		func(0, n - 1);
+		long long ans = 0;
+		for (int i = 0; i < n; ++i) {
+			long long tmp = (long long)v[i].first * (long long)v[i].second;
+			if (ans < tmp)
+				ans = tmp;
+		}
+		cout << ans << endl;
+	}
+}
