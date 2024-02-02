b814ac0d5294f4155a1e184d6734669453078c61
Hiver93 <90398836+Hiver93@users.noreply.github.com>
2024-02-02 21:50:53 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+int c;
+
+bool cmp(vector<int> a, vector<int> b){
+    if(a[c] < b[c]){
+        return 1;
+    }
+    if(a[c] == b[c]){
+        return a[0] > b[0];
+    }
+    return 0;
+}
+
+int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
+    int answer = 0;
+    c = col-1;
+    
+    sort(data.begin(),data.end(),cmp);
+    for(int i = row_begin-1; i < row_end; ++i){
+        int tmp = 0;
+        for(int j = 0; j < data[0].size(); ++j){
+            tmp += data[i][j] % (i+1);
+        }
+        answer ^= tmp;
+    }
+    
+    
+    return answer;
+}
