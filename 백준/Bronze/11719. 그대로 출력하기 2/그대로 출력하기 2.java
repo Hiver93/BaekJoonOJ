c68b17bb7964d0f343346ddc27fa11c011cef10d
Hiver93 <90398836+Hiver93@users.noreply.github.com>
2024-04-08 17:38:24 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 116 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		String str;
+		while(sc.hasNextLine()) {
+			sb.append(sc.nextLine());
+            sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+}
