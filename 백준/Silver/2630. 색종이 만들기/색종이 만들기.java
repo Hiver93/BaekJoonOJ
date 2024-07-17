19760deeb0e75acc259cf311b1260273542fcbd2
Hiver93 <90398836+Hiver93@users.noreply.github.com>
2024-07-17 23:46:31 +0900

    [Silver II] Title: 색종이 만들기, Time: 104 ms, Memory: 13644 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,39 +1,64 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] arr = new int[128][128];
-	static int[] ans = new int[2];
-	static void func(int s1, int e1, int s2, int e2)
-	{
-	    int current = arr[s1][s2];
-	    for(int i = s1; i < e1; ++i)
-	    {
-	        for(int j = s2; j < e2; ++j)
-	        {
-	            if(arr[i][j] != current){
-	                func(s1, (s1+e1)/2, s2,(s2+e2)/2);
-	                func(s1, (s1+e1)/2,(s2+e2)/2,e2);
-	                func((s1+e1)/2, e1, s2, (s2+e2)/2);
-	                func((s1+e1)/2, e1, (s2+e2)/2, e2);
-	                return;
-	            }
-	        }
-	    }
-	    ans[arr[s1][s2]]++;
-	}
-	public static void main(String[] args) {
+	static int[][] arr;
+	static int ans = 0;
+	static int blue = 0;
+	public static int func(int y, int x, int w) {
+		if(w == 1) {
+			return arr[y][x]; 
+		}
 		
-		Scanner sc = new Scanner(System.in);
-	    int n = sc.nextInt();	    
-	    for(int i = 0; i < n; ++i)
-	    {
-	        for(int j = 0; j < n; ++j)
-	        {
-	            arr[i][j] = sc.nextInt();
-	        }
-	     }
-	    func(0,n,0,n);
-	    System.out.printf("%d\n%d\n",ans[0],ans[1]);	    
-	    
+		int[] tmp = new int[4];
+		tmp[0] = func(y,x,w/2);
+		tmp[1] = func(y,x + w/2, w/2);
+		tmp[2] = func(y+w/2,x, w/2);
+		tmp[3] = func(y+w/2,x+w/2,w/2);
+		boolean isOk = true;
+		int cnt = 0;
+		for(int i = 0; i < 4; ++i) {
+			if(tmp[i] == -1) {
+				isOk = false;
+				continue;
+			}
+			if(tmp[i] != tmp[0]){
+				isOk = false;
+			}
+			cnt++;
+		}
+		if(!isOk) {
+			for(int i = 0; i < 4; ++i) {
+				if(tmp[i] == 1)
+					blue++;
+			}
+			ans += cnt;
+			return -1;
+		}
+		return tmp[0];
+	
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.valueOf(br.readLine());
+		arr = new int[n][n];
+		for(int i = 0; i < n; ++i) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < n; ++j) {
+				arr[i][j] = st.nextToken().charAt(0)-'0';
+			}
+		}
+		int num = func(0,0,n); 
+		if(num != -1) {;
+			ans++;
+			if(num == 1)
+				blue++;
+		}
+		System.out.println(ans - blue);
+		System.out.println(blue);
+		
 	}
 }
