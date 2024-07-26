3a71da45d660251fd9cc9b3ae98cf2198497cf9d
Hiver93 <90398836+Hiver93@users.noreply.github.com>
2024-07-26 14:43:31 +0900

    [Silver III] Title: 이친수, Time: 112 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		long[] arr = new long[n+2];
+		arr[1] = 1;
+		arr[2] = 1;
+		for(int i = 3; i <= n; ++i) {
+			arr[i] = arr[i-2] + arr[i-1];
+		}
+		System.out.println(arr[n]);
+	}
+}
